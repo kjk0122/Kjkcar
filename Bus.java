221936482e4c3888638dc9698c8c71d9195f8aa1
kjk0122 <juke9548@gmail.com>
2022-12-23 16:15:31 +0900

    - 버스 번호         - 버스 객체 생성시 번호는 고유값으로 생성되어야 합니다.			-UUID를 통해 고유번호 생성     - 버스 상태 변경         - 버스 객체 생성시 최초 상태는 **‘운행’** 상태가 되며			UUID 생성하면서 같이 병행하게 만듦         - 주유량이 떨어지거나, 운행을 종료할 때 **‘차고지행’** 상태로 변경 합니다.		O         - 10미만일 경우 ‘주유가 필요하다’는 메시지를 출력해 주세요				O     - 승객 탑승         - 승객 탑승은 **‘최대 승객수’** 이하까지 가능하며 **‘운행 중’**인 상태의 버스만 가능합니다.   O         - 탑승시 현재 승객수가 증가되어야 합니다.					    O     - 속도 변경         - 주유 상태를 체크하고 주유량이 10 이상이어야 운행할 수 있습니다.			O - 경고메시지 - 주유량을 확인해 주세요.							O - print문으로 출력							O - 변경할 속도를 입력 받아 현재 속도에 추가 하거나 뺄 수 있어야 합니다.                                   O

--- Bus.java
+++ Bus.java
@@ -1,21 +1,72 @@
 package car;
 
+import com.sun.nio.sctp.PeerAddressChangeNotification;
+
+import java.util.UUID;
+
 public class Bus {//        - 포함해야 할 정보
-    int maxCustomer;//        1. 최대 승객수
-    int minCustomer; //        2. 현재 승객수
+    int maxCustomer;//        1.  승객수
+    int Customer; //        2. 현재 승객수
     int fare;           //        3. 요금
-    int busNumber;  //        4. 버스 번호
-    int fuelAmount; //        5. 주유량
-    int speed=0;     //        6. 현재 속도
+    String busuid;  //        4. 버스 번호
+
+    int fuelAmount=30; //        5. 주유량
+    int speed = 0;     //        6. 현재 속도
     String[] status = {"운행", "차고지 행"}; //          1.  운행, 차고지 행
+    String current = "";  //현재상태
 
-
-    public int[] setCustomer(int min, int max){
-        this.minCustomer=min;
-        this.maxCustomer=max;
-        return new int[]{minCustomer, maxCustomer};
+    public void setInfo(int max, int fare, int spd) {
+        this.maxCustomer = max;
+        this.fare = fare;
+        this.speed=spd;
+        String uuid = UUID.randomUUID().toString();
+        this.busuid=uuid; this.current = status[0];
+        System.out.println("임시 아이디가 생성되었습니다");
     }
 
+    public int getSpeed() {
+        return this.speed;
+    }
 
+    public void chSpd(int ud) {
+        if(this.totalWarn(this.Customer,this.fuelAmount)==0){
+            this.speed=this.speed+ud;
+        }
+    }
 
+    public int fuelWarn(int f) {
+        this.fuelAmount = f;
+        if (this.fuelAmount <= 0) {
+            current = status[1];
+            System.out.println("현재 연료가 "+this.fuelAmount+"이라서 장비를 정지합니다.\n"+this.current);
+            return 1;
+        } else if (this.fuelAmount < 10) {
+            System.out.println("주유가 필요하다");
+        } else{
+            System.out.println("기름 양 OK" + this.fuelAmount);
+        }
+        return 0;
+    }
+    public int overWarn(int Customer) {
+        if(this.current=="운행") {
+            this.Customer = Customer;
+            if (this.Customer > this.maxCustomer) {
+                System.out.println("정원 초과");
+                return 1;
+            }else {
+                System.out.println("잔여석 있음");
+
+            }
+        }
+        return 0;
+    }
+    public int totalWarn(int c, int f){
+        if(this.overWarn(c)==0&&this.fuelWarn(f)==0){
+            System.out.println("이상 없음!");
+            return 0;
+        }else{
+            System.out.println("차에 문제가 있습니다!");
+            return 1;
+        }
+    }
 }
--- mycompany.java
+++ mycompany.java
@@ -3,7 +3,12 @@ package car;
 public class mycompany {
     public static void main(String[] args) {
         Bus bus = new Bus();
-        bus.setCustomer(200,5000);
-        System.out.println(bus.minCustomer+", "+bus.maxCustomer);
+        Bus bus1 = new Bus();
+        bus.setInfo(100,300, 30);
+        bus.chSpd(5);
+        System.out.println(bus.getSpeed());
+
+
+//        bus.setWarn(0); 연료 경고
     }
 }
