4aaf511196c4a18180d950cdbb58a293821a18d4
kjk0122 <juke9548@gmail.com>
2022-12-23 13:23:04 +0900

    올려놓기

---
+++ Bus.java
@@ -0,0 +1,12 @@
+package car;
+
+public class Bus {//        - 포함해야 할 정보
+    int maxCustomer;//        1. 최대 승객수
+    int minCustomer; //        2. 현재 승객수
+    int fare;           //        3. 요금
+    int busNumber;  //        4. 버스 번호
+    int fuelAmount; //        5. 주유량
+    int speed;     //        6. 현재 속도
+    String[] status = {//        7. 상태
+    "운행", "차고지 행"}; //          1.  운행, 차고지 행
+}
